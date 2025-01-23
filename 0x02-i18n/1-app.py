7b0bb4a7adba2987d89542a0dbe934015355076e
Mawuawoe <desmonddzakago@gmail.com>
2025-01-23 10:36:54 +0000

    task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+"""
+flask app with bable
+"""
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 app = Flask(__name__)
+
 
 class Config:
     """Configuration for Flask-Babel."""
@@ -10,15 +15,18 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app.config.from_object(Config)
 
 # Instantiate Babel
 babel = Babel(app)
 
+
 @app.route('/')
 def index():
     """Render the index page."""
     return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
